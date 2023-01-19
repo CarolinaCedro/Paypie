c74b0809796a123a0109526038dd1c59dca20d1b
CarolinaCedro <yotechgirl@gmail.com>
2023-01-19 13:40:06 -0300

    feat: first commit

---
+++ src/main/java/io/CarolinaCedro/paypies/PaypiesApplication.java
@@ -0,0 +1,13 @@
+package io.CarolinaCedro.paypies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaypiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaypiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/CarolinaCedro/paypies/PaypiesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.CarolinaCedro.paypies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaypiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
